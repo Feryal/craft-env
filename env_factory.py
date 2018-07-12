8c3b3a670797c1c7d4624b139f4fc8f970d48570
Feryal <feryal.mp@gmail.com>
2018-07-12 17:59:12 +0900

    Change indent from 4 to 2

--- env_factory.py
+++ env_factory.py
@@ -16,55 +16,55 @@ Task = collections.namedtuple("Task", ["goal", "steps"])
 
 
 class EnvironmentFactory(object):
-    """Factory instantiating Craft environments."""
+  """Factory instantiating Craft environments."""
 
-    def __init__(self, recipes_path, hints_path, seed=0):
-        self.subtask_index = util.Index()
-        self.task_index = util.Index()
+  def __init__(self, recipes_path, hints_path, seed=0):
+    self.subtask_index = util.Index()
+    self.task_index = util.Index()
 
-        # create World
-        self.world = craft.CraftWorld(recipes_path, seed)
+    # create World
+    self.world = craft.CraftWorld(recipes_path, seed)
 
-        # Load the tasks with sub-steps (== hints)
-        with open(hints_path) as hints_f:
-            self.hints = yaml.load(hints_f)
+    # Load the tasks with sub-steps (== hints)
+    with open(hints_path) as hints_f:
+      self.hints = yaml.load(hints_f)
 
-        # Setup all possible tasks
-        self._init_tasks()
+    # Setup all possible tasks
+    self._init_tasks()
 
-    def _init_tasks(self):
-        """Build the list of tasks and subtasks."""
-        # organize task and subtask indices
-        self.tasks_by_subtask = collections.defaultdict(list)
-        self.tasks = {}
-        for hint_key, hint in self.hints.items():
-            # hint_key: make[plank], hint/steps: get_wood, makeAtToolshed
-            goal = util.parse_fexp(hint_key)
-            # goal: (make, plank)
-            goal = (self.subtask_index.index(goal[0]),
-                    self.world.cookbook.index[goal[1]])
-            steps = tuple(self.subtask_index.index(s) for s in hint)
-            task = Task(goal, steps)
-            for subtask in steps:
-                self.tasks_by_subtask[subtask].append(task)
+  def _init_tasks(self):
+    """Build the list of tasks and subtasks."""
+    # organize task and subtask indices
+    self.tasks_by_subtask = collections.defaultdict(list)
+    self.tasks = {}
+    for hint_key, hint in self.hints.items():
+      # hint_key: make[plank], hint/steps: get_wood, makeAtToolshed
+      goal = util.parse_fexp(hint_key)
+      # goal: (make, plank)
+      goal = (self.subtask_index.index(goal[0]),
+          self.world.cookbook.index[goal[1]])
+      steps = tuple(self.subtask_index.index(s) for s in hint)
+      task = Task(goal, steps)
+      for subtask in steps:
+        self.tasks_by_subtask[subtask].append(task)
 
-            self.tasks[hint_key] = task
-            self.task_index.index(task)
+      self.tasks[hint_key] = task
+      self.task_index.index(task)
 
-        self.task_names = self.tasks.keys()
+    self.task_names = self.tasks.keys()
 
-    def sample_environment(self, task_name=None):
-        if task_name is None:
-            task_name = np.random.choice(self.task_names)
+  def sample_environment(self, task_name=None):
+    if task_name is None:
+      task_name = np.random.choice(self.task_names)
 
-        # Get the task
-        task = self.tasks[task_name]
-        goal_arg = task.goal[1]
+    # Get the task
+    task = self.tasks[task_name]
+    goal_arg = task.goal[1]
 
-        # Sample a world (== scenario for them...)
-        scenario = self.world.sample_scenario_with_goal(goal_arg)
+    # Sample a world (== scenario for them...)
+    scenario = self.world.sample_scenario_with_goal(goal_arg)
 
-        # Wrap it into an environment
-        environment = env.CraftLab(scenario, task_name, task)
+    # Wrap it into an environment
+    environment = env.CraftLab(scenario, task_name, task)
 
-        return environment
+    return environment
--- run_random_agent.py
+++ run_random_agent.py
@@ -11,40 +11,40 @@ import env_factory
 
 
 def run_loop(env, n_steps, render=False):
-    possible_actions = env.action_specs()
+  possible_actions = env.action_specs()
 
-    env.reset()
+  env.reset()
 
-    for t in xrange(n_steps):
-        # Random action
-        action = np.random.choice(possible_actions.values())
+  for t in xrange(n_steps):
+    # Random action
+    action = np.random.choice(possible_actions.values())
 
-        reward, done, observations = env.step(action)
+    reward, done, observations = env.step(action)
 
-        if render:
-            curses.wrapper(env.render(fps=15))
-        else:
-            print("[{}] reward={} done={} \n observations: {}".format(
-                t, reward, done, observations))
+    if render:
+      curses.wrapper(env.render(fps=15))
+    else:
+      print("[{}] reward={} done={} \n observations: {}".format(
+        t, reward, done, observations))
 
-        if done:
-            print("Woohooo did something!")
+    if done:
+      print("Woohooo did something!")
 
 
 def main():
-    recipes_path = "resources/recipes.yaml"
-    hints_path = "resources/hints.yaml"
-    envSampler = env_factory.EnvironmentFactory(
-        recipes_path, hints_path, seed=1)
+  recipes_path = "resources/recipes.yaml"
+  hints_path = "resources/hints.yaml"
+  envSampler = env_factory.EnvironmentFactory(
+    recipes_path, hints_path, seed=1)
 
-    env = envSampler.sample_environment()
-    print("Environment: task {}: {}".format(env.task_name, env.task))
-    run_loop(env, 1000, render=True)
+  env = envSampler.sample_environment()
+  print("Environment: task {}: {}".format(env.task_name, env.task))
+  run_loop(env, 1000, render=True)
 
-    # env = envSampler.sample_environment_by_name('make[plank]')
-    # print("Environment: task {}: {}".format(env.task_name, env.task))
-    # run_loop(env, 10)
+  # env = envSampler.sample_environment_by_name('make[plank]')
+  # print("Environment: task {}: {}".format(env.task_name, env.task))
+  # run_loop(env, 10)
 
 
 if __name__ == '__main__':
-    main()
+  main()
